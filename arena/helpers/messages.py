ea70a5dc3299cf226b12191abf42cb09ead3088d
Ömer DEMİRARSLAN <omerdemirarsln@gmail.com>
2021-07-10 18:26:01 +0300

    #0010 #command: Bug Fix

--- arena/helpers/messages.py
+++ arena/helpers/messages.py
@@ -7,6 +7,6 @@ NULL_AREA = "There Is Null Are Your Entered Information. Pleas Enter All Informa
 SEARCH_KEY_ERROR = "User Search Keyword Invalid"
 USER_NOT_FOUND = "User Not Found"
 HOME_PAGE = "Welcome To Arena Project. If you want test this django REST API: " \
-            "Use the fallowing API reques options.\n\nlocalhost/users/sign-up/\nlocalhost/users/me/ \n" \
+            "Use the fallowing API request options.\n\nlocalhost/users/sign-up/\nlocalhost/users/me/ \n" \
             "localhost/users/search/username=iron-man \nlocalhost/users/update/ \n\n" \
             "THANK YOU FOR FREE TIME"
